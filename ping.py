b4f8bc06af1204d135d595254010e5051305dbe5
Romet Reino <rreino@ttu.ee>
2022-05-26 12:08:53 +0300

    ping.py fixed

--- ping.py
+++ ping.py
@@ -1,8 +1,11 @@
 import requests
 
+
 def check_server_status():
-    a = requests.get(url)
-    if a = 200:
+    a = requests.get("http://itcollege.ee")
+    if a.status_code == 200:
         return True
     else:
         return False
+
+print(check_server_status())
